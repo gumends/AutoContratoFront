1b7c47c35fa708dcd6c7fe84dec5ed1e80107cc9
gumends <gustavo08.mendes@gmail.com>
2025-02-22 00:08:06 -0300

    Pagina de Usuarios

---
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainContantComponent } from "../../components/main-contant/main-contant.component";
+
+@Component({
+  selector: 'app-usuarios',
+  imports: [MainContantComponent],
+  templateUrl: './usuarios.component.html',
+  styleUrl: './usuarios.component.css'
+})
+export class UsuariosComponent {
+
+}
