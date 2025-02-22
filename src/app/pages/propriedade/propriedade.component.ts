51649b66bd1c27f17b7c91f5765a27510015e74f
gumends <gustavo08.mendes@gmail.com>
2025-02-22 00:07:51 -0300

    Página de proprietario

---
+++ src/app/pages/propriedade/propriedade.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainContantComponent } from "../../components/main-contant/main-contant.component";
+
+@Component({
+  selector: 'app-propriedade',
+  imports: [MainContantComponent],
+  templateUrl: './propriedade.component.html',
+  styleUrl: './propriedade.component.css'
+})
+export class PropriedadeComponent {
+
+}
---
+++ src/app/pages/proprietario/proprietario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainContantComponent } from "../../components/main-contant/main-contant.component";
+
+@Component({
+  selector: 'app-proprietario',
+  imports: [MainContantComponent],
+  templateUrl: './proprietario.component.html',
+  styleUrl: './proprietario.component.css'
+})
+export class ProprietarioComponent {
+
+}
