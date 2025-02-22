93f7ec242ca77c16ebb75957c50bf8dfebddbcc6
gumends <gustavo08.mendes@gmail.com>
2025-02-22 00:05:24 -0300

    modo light e dark

---
+++ src/app/components/theme-mode/theme-mode.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { AppComponent } from "../icons/moon/moon.component";
+
+@Component({
+  selector: 'app-theme-mode',
+  imports: [AppComponent],
+  templateUrl: './theme-mode.component.html',
+  styleUrls: ['./theme-mode.component.css'],
+})
+export class ThemeModeComponent {
+  constructor(private themeService: ThemeService) {}
+
+  typeIcon: string = "heroMoonSolid";
+  localstorege: Storage = localStorage;
+  theme: string = "";
+
+  ngOnInit() {
+    this.theme = this.localstorege.getItem('theme')?.toString() || "light";
+    this.theme === "dark" ? this.typeIcon = "heroSunSolid" : this.typeIcon = "heroMoonSolid";
+  }
+  toggleTheme(): void {
+    this.typeIcon === "heroMoonSolid" ? this.typeIcon = "heroSunSolid" : this.typeIcon = "heroMoonSolid";
+    this.themeService.toggleTheme();
+  }
+}
