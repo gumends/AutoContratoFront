2113898d584eb45a3ff53e734b0773d1b8207839
gumends <gustavo08.mendes@gmail.com>
2025-02-26 02:40:21 -0300

    mascara de rg

---
+++ src/app/directives/rg-mask.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appRgMask]'
+})
+export class RgMaskDirective {
+  constructor(private el: ElementRef) {}
+
+  @HostListener('input', ['$event'])
+  onInputChange(event: any) {
+    let value = event.target.value.replace(/\D/g, ''); // Remove tudo que não é número
+
+    if (value.length > 9) value = value.substring(0, 9);
+
+    // Aplica a máscara XX.XXX.XXX-X
+    if (value.length > 7) {
+      value = value.replace(/^(\d{2})(\d{3})(\d{3})(\d{1})$/, '$1.$2.$3-$4');
+    } else if (value.length > 4) {
+      value = value.replace(/^(\d{2})(\d{3})(\d{0,3})$/, '$1.$2.$3');
+    } else if (value.length > 2) {
+      value = value.replace(/^(\d{2})(\d{0,3})$/, '$1.$2');
+    }
+
+    this.el.nativeElement.value = value;
+  }
+}
