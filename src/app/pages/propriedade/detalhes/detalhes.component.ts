059af5a48d6f5123f891d425bdb81bc50ae80a16
gumends <gustavo08.mendes@gmail.com>
2025-02-26 23:52:56 -0300

    Tela de propriedade

---
+++ src/app/pages/propriedade/detalhes/detalhes.component.ts
@@ -0,0 +1,157 @@
+import { Component, OnInit } from '@angular/core';
+import { MainContantComponent } from "../../../components/main-contant/main-contant.component";
+import { AppComponent } from "../../../components/icons/moon/moon.component";
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmSelectImports } from '@spartan-ng/ui-select-helm';
+import { BrnSelectImports } from '@spartan-ng/brain/select';
+import { ButtonPreviewComponent } from "../../../components/button/button.component";
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PropriedadeService } from '../../../services/propriedade.service';
+import { IPropriedadeContent } from '../../../Types/propriedade';
+import { NgFor } from '@angular/common';
+import { RgMaskDirective } from '../../../directives/rg-mask.directive';
+import { ToastrService } from 'ngx-toastr';
+import { LocatarioService } from '../../../services/locatario.service';
+import { Router } from '@angular/router';
+import { CurrencyMaskDirective } from '../../../directives/real.directive';
+import { ProprietarioService } from '../../../services/proprietario.service';
+import { ILocatarioContent } from '../../../Types/LocatarioResponse';
+import { IContentProprietario } from '../../../Types/Proprietario';
+@Component({
+  selector: 'app-detalhes',
+  imports: [
+    MainContantComponent,
+    HlmInputDirective,
+    HlmLabelDirective,
+    BrnSelectImports,
+    HlmSelectImports,
+    ButtonPreviewComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    NgFor,
+    RgMaskDirective,
+    CurrencyMaskDirective
+  ],
+  templateUrl: './detalhes.component.html',
+  styleUrl: './detalhes.component.css'
+})
+export class DetalhesPropriedadeComponent implements OnInit {
+
+  rua: string = '';
+  numero!: number;
+  bairro: string = '';
+  cep: string = '';
+  localizacao: string = '';
+  aluguel: string = 'R$ 0,00';
+  dataPagamento: Date = new Date();
+  proprietarioId: string = '';
+  locatarioId: string = '';
+  status: boolean = true;
+
+  locatarios: ILocatarioContent[] = [];
+  proprietarios: IContentProprietario[] = [];
+
+  id: string = '';
+
+
+  constructor(
+    private fb: FormBuilder,
+    private servicePropriedade: PropriedadeService,
+    private toastr: ToastrService,
+    private serviceProprietario: ProprietarioService,
+    private serviceLocatario: LocatarioService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      rua: [this.rua, [Validators.required, Validators.minLength(6)]],
+      numero: [this.numero, [Validators.required, Validators.minLength(11)]],
+      bairro: [this.bairro, [Validators.required, Validators.minLength(1)]],
+      cep: [this.cep, [Validators.required, Validators.minLength(11)]],
+      localizacao: [this.localizacao, [Validators.required]],
+      aluguel: [this.aluguel, [Validators.required]],
+      dataPagamento: [this.dataPagamento, [Validators.required]],
+      proprietarioID: [this.proprietarioId, [Validators.required]],
+      status: [this.status, [Validators.required]],
+    });
+
+  }
+
+  ngOnInit() {
+    this.serviceProprietario.buscarProprietarios(0, true, '').subscribe({
+      next: (response) => {
+        this.proprietarios = response.content;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+    this.serviceLocatario.buscarLocatarios(0, true, '').subscribe({
+      next: (response) => {
+        this.locatarios = response.content;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+    this.buscarLocatario();
+  }
+
+  form: FormGroup;
+
+  get selectedValue() {
+    return this.form.get('propriedadeId')?.value;
+  }
+
+  salvar() {
+    if (this.id) {
+      this.servicePropriedade.atualizarPropriedade(this.id, this.form.value).subscribe({
+        next: (response) => {
+          if (this.form.valid) {
+            this.toastr.success('Propriedade atualizada com sucesso', 'Atualizado!');
+            this.form.reset();
+            this.router.navigate(['/propriedade']);
+          } else {
+            this.toastr.success('Formulário inválido', 'Erro!');
+          }
+        },
+        error: (error) => {
+          this.toastr.error('Não foi possivel realizar o cadastro', 'Erro!');
+          console.log(error);
+        }
+      })
+    } else {
+      console.log(this.form.value);
+      this.servicePropriedade.criarPropriedade(this.form.value).subscribe({
+        next: (response) => {
+          if (this.form.valid) {
+            this.toastr.success('Propriedade cadastrada com sucesso', 'Cadastro!');
+            this.form.reset();
+            this.router.navigate(['/propriedade']);
+          } else {
+            this.toastr.success('Formulário inválido', 'Erro!');
+          }
+        },
+        error: (error) => {
+          this.toastr.error('Não foi possivel realizar o cadastro', 'Erro!');
+          console.log(error);
+        }
+      })
+    }
+  }
+
+  buscarLocatario() {
+    this.id = this.router.url.split("/")[3];
+    if (this.id) {
+      this.servicePropriedade.buscarPropriedade(this.id).subscribe({
+        next: (response) => {
+          this.form.patchValue(response);
+        },
+        error: (error) => {
+          this.toastr.error('Não foi possivel realizar o cadastro', 'Erro!');
+          console.log(error);
+        }
+      })
+    }
+  }
+}
--- src/app/pages/propriedade/propriedade.component.ts
+++ src/app/pages/propriedade/propriedade.component.ts
@@ -1,12 +1,142 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, OnChanges } from '@angular/core';
 import { MainContantComponent } from "../../components/main-contant/main-contant.component";
-
+import { NgClass, NgFor } from '@angular/common';
+import { FormatarDataPipe } from '../../pipes/data.pipe';
+import { AppComponent } from "../../components/icons/moon/moon.component";
+import { Subject } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { takeUntil } from 'rxjs/operators';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HlmSelectImports } from '@spartan-ng/ui-select-helm';
+import { BrnSelectImports } from '@spartan-ng/brain/select';
+import { PropriedadeService } from '../../services/propriedade.service';
+import { IPropriedadeContent, IPropriedadePaginado } from '../../Types/propriedade';
+import { ToastrService } from 'ngx-toastr';
+import { HlmBadgeDirective } from '@spartan-ng/ui-badge-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { BrnAlertDialogContentDirective, BrnAlertDialogTriggerDirective } from '@spartan-ng/brain/alert-dialog';
+import {
+  HlmAlertDialogActionButtonDirective,
+  HlmAlertDialogComponent,
+  HlmAlertDialogContentComponent,
+  HlmAlertDialogDescriptionDirective,
+  HlmAlertDialogFooterComponent,
+  HlmAlertDialogHeaderComponent,
+  HlmAlertDialogTitleDirective,
+} from '@spartan-ng/ui-alertdialog-helm';
+import { ContratoComponent } from "../../components/contrato/contrato.component";
 @Component({
-  selector: 'app-propriedade',
-  imports: [MainContantComponent],
+  selector: 'app-locatario',
+  standalone: true,
+  imports: [
+    MainContantComponent,
+    NgFor,
+    FormatarDataPipe,
+    AppComponent,
+    HlmButtonDirective,
+    ReactiveFormsModule,
+    FormsModule,
+    HlmSelectImports,
+    BrnSelectImports,
+    HlmBadgeDirective,
+    HlmInputDirective,
+    HlmAlertDialogComponent,
+    BrnAlertDialogTriggerDirective,
+    BrnAlertDialogContentDirective,
+    HlmAlertDialogComponent,
+    HlmAlertDialogContentComponent,
+    HlmAlertDialogHeaderComponent,
+    HlmAlertDialogTitleDirective,
+    HlmAlertDialogDescriptionDirective,
+    HlmAlertDialogFooterComponent,
+    HlmAlertDialogActionButtonDirective,
+    RouterLink,
+    NgClass,
+    ContratoComponent
+],
   templateUrl: './propriedade.component.html',
   styleUrl: './propriedade.component.css'
 })
-export class PropriedadeComponent {
+export class PropriedadeComponent implements OnInit, OnDestroy, OnChanges {
 
+  conteudo: IPropriedadeContent[] = [];
+  pagina!: number;
+  total!: number;
+  propriedades: IPropriedadeContent[] = [];
+  status: boolean = true;
+  rua: string = '';
+
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private authService: AuthService,
+    private servicePropriedade: PropriedadeService,
+    private toastr: ToastrService
+  ) {
+  }
+  ngOnChanges(changes: any): void {
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    this.carregarDados();
+    this.authService.accountChanged$.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.carregarDados();
+    });
+    this.carregarDados();
+  }
+
+  buscar() {
+    this.carregarDados();
+  }
+
+  desativar(id: string) {
+    this.servicePropriedade.desativarPropriedade(id).subscribe({
+      next: (res: IPropriedadeContent) => {
+          if(res.status === false){
+            this.carregarDados();
+            this.toastr.warning('Locatário desativado com sucesso', 'Desativado!');
+          } else {
+            this.carregarDados();
+            this.toastr.success('Locatário ativado com sucesso', 'Ativado!');
+          }
+      },
+      error: (error) => {
+        console.log(error);
+        this.toastr.error('Erro ao desativar locatário', 'Erro!');
+      }
+    });
+  }
+
+  carregarDados() {
+    this.servicePropriedade.buscarPropriedades(this.status, this.rua).subscribe({
+      next: (res: IPropriedadePaginado) => {
+        console.log(res.content);
+        
+        this.conteudo = res.content;
+        this.pagina = res.number;
+        this.total = res.totalPages;
+      },
+      error: (error) => {
+        console.error('Erro ao carregar locatários:', error);
+      }
+    });
+  }
+
+  onStatus(){
+    this.carregarDados();
+  }
+
+  recarregar() {
+    this.carregarDados();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
