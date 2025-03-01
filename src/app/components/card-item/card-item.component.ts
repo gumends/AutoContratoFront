f0de17c676b7e64cae3cb5ae9fd584b5e64cc963
gumends <gustavo08.mendes@gmail.com>
2025-02-28 23:16:46 -0300

    Card de itens da tela home

---
+++ src/app/components/card-item/card-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { AppComponent } from "../icons/moon/moon.component";
+
+@Component({
+  selector: 'app-card-item',
+  imports: [AppComponent],
+  templateUrl: './card-item.component.html',
+  styleUrl: './card-item.component.css'
+})
+export class CardItemComponent {
+  @Input() icone: string = ""
+  @Input() text_main: string = ""
+  @Input() text_sub: string = ""
+  @Input() value: string = ""
+}
