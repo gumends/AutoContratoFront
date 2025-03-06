44a842737c8e9c6fad68e8924d6eb654b70326cb
gumends <gustavo08.mendes@gmail.com>
2025-03-05 23:59:42 -0300

    Construção da pagina home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MainContantComponent } from "../../components/main-contant/main-contant.component";
 
 import { HlmTableModule } from "../../../../libs/ui/ui-table-helm/src/index";
+import { CardComponent } from "../../components/card/card.component";
+import { CardItemComponent } from "../../components/card-item/card-item.component";
+import { LocatarioService } from '../../services/locatario.service';
+import { PropriedadeService } from '../../services/propriedade.service';
+import { ProprietarioService } from '../../services/proprietario.service';
+import { ILocatarioContent } from '../../Types/LocatarioResponse';
+import { IContentProprietario } from '../../Types/Proprietario';
+import { IPropriedadeContent } from '../../Types/propriedade';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   
-  imports: [MainContantComponent, HlmTableModule],
+  imports: [MainContantComponent, HlmTableModule, CardComponent, CardItemComponent, NgFor, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(
+    private locatarioService: LocatarioService,
+    private propriedadeService: PropriedadeService,
+    private proprietarioService: ProprietarioService
+  ) {}
+
+  locararios: ILocatarioContent[] = []
+  proprietarios: IContentProprietario[] = []
+  propriedades: IPropriedadeContent[] = []
+  casaDisponiveis: number = 0;
+  casaOcupadas: number = 0;
+
+
+  ngOnInit(): void {
+    this.buscarLocatarios()
+    this.buscarProprietarios()
+    this.buscarPropriedades()
+  }
+
+  buscarLocatarios(){
+    this.locatarioService.buscarLocatarios(0, true, '').subscribe({
+      next: (response) => {
+        this.locararios = response.content;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+  buscarProprietarios(){
+    this.proprietarioService.buscarProprietarios(0, true, '').subscribe({
+      next: (response) => {
+        this.proprietarios = response.content;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+  buscarPropriedades(){
+    this.propriedadeService.buscarPropriedades().subscribe({
+      next: (response) => {
+        this.propriedades = response.content;
+        this.casaDisponiveis = this.propriedades.filter(propriedade => propriedade.alugada === false).length;
+        this.casaOcupadas = this.propriedades.filter(propriedade => propriedade.alugada === true).length;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
 }
