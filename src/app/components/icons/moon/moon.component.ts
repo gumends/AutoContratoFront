9a676f179aa9efa8891557d7c0702fd565219548
gumends <gustavo08.mendes@gmail.com>
2025-03-12 23:52:17 -0300

    Novos icones

--- src/app/components/icons/moon/moon.component.ts
+++ src/app/components/icons/moon/moon.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { featherAirplay } from '@ng-icons/feather-icons';
-import { heroAcademicCap, heroArrowPath, heroCurrencyDollar, heroExclamationTriangle, heroHomeModern, heroMagnifyingGlass, heroPencil, heroPlus, heroTrash, heroXCircle } from '@ng-icons/heroicons/outline';
+import { heroAcademicCap, heroArrowLeft, heroArrowPath, heroArrowRight, heroCurrencyDollar, heroExclamationTriangle, heroHomeModern, heroMagnifyingGlass, heroPencil, heroPlus, heroTrash, heroXCircle } from '@ng-icons/heroicons/outline';
 import { heroArrowSmallLeftSolid, heroArrowSmallRightSolid, heroCheckSolid, heroCurrencyDollarSolid, heroDocumentTextSolid, heroHomeModernSolid, heroHomeSolid, heroMoonSolid, heroPencilSolid, heroSunSolid, heroTrashSolid, heroUserCircleSolid, heroUserGroupSolid, heroUserPlusSolid, heroUserSolid, heroXMarkSolid } from '@ng-icons/heroicons/solid';
 import { Input } from '@angular/core';
 import { NgStyle } from '@angular/common';
@@ -40,7 +40,9 @@ import { ionExit, ionPrint } from '@ng-icons/ionicons';
             heroCurrencyDollar,
             heroCurrencyDollarSolid,
             heroTrashSolid,
-            heroExclamationTriangle
+            heroExclamationTriangle,
+            heroArrowLeft,
+            heroArrowRight
         })]
 })
 export class AppComponent {
